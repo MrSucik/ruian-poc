5504ba8e04169007b008e8f798e69fddf2b3c5bd
Daniel Suchan <mr.sucik@gmail.com>
2024-05-22 13:33:59 +0200

    feat: add address form with validations

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
 "use client";
 
-import styles from "./page.module.css";
-import React from "react";
+import React, { useState } from "react";
 import { Formik } from "formik";
 
+const validate = (values: { address: string }) => {
+  const errors: { address?: string } = {};
+  if (!values.address) {
+    errors.address = "Address is required.";
+  }
+  return errors;
+};
+
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <main className={styles.main}>
+    <main className="container grid place-items-center h-screen mx-auto">
       <Formik
         initialValues={{ address: "" }}
-        validate={(values) => {
-          console.log("values", values);
-
-          const errors: { address?: string } = {};
-          if (!values.address) {
-            errors.address = "Required";
-          }
-          return errors;
-        }}
+        validate={validate}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
@@ -33,19 +34,83 @@ export default function Home() {
           handleBlur,
           handleSubmit,
           isSubmitting,
+          setFieldValue,
         }) => (
-          <form onSubmit={handleSubmit} className={styles.form}>
-            <div className={styles.field}>
-              <input
-                type="address"
-                name="address"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.address}
-              />
-              {errors.address && touched.address && errors.address}
-            </div>
-            <button type="submit" disabled={isSubmitting}>
+          <form onSubmit={handleSubmit} className="grid gap-2 w-full">
+            <details className="dropdown" open={open}>
+              <summary>
+                <input
+                  type="address"
+                  name="address"
+                  className="input input-primary w-full"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  onFocus={() => setOpen(true)}
+                  value={values.address}
+                />
+              </summary>
+              <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                <li>
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => {
+                      setFieldValue("address", "xx");
+                      setOpen(false);
+                    }}
+                    type="button"
+                  >
+                    Address 1
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => {
+                      setFieldValue("address", "xx");
+                      setOpen(false);
+                    }}
+                    type="button"
+                  >
+                    Address 2
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => {
+                      setFieldValue("address", "xx");
+                      setOpen(false);
+                    }}
+                    type="button"
+                  >
+                    Address 3
+                  </button>
+                </li>
+              </ul>
+            </details>
+            {errors.address && touched.address && (
+              <div role="alert" className="alert alert-error p-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>{errors.address}</span>
+              </div>
+            )}
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="btn btn-primary"
+            >
               Submit
             </button>
           </form>
